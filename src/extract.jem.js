f50e503438d07a7f0bb61799bacba084a4fc6a9a
RAZAFINJATOVO Josia <rjosia@gmail.com>
2025-06-08 15:35:22 +0200

    Update count JEM collection

--- src/extract.jem.js
+++ src/extract.jem.js
@@ -2,7 +2,7 @@ import {createFile} from "./utils.js";
 import {extractLyricsCollections, extractLyricsFromJemaf, fetchFormattedLyrics} from "./fetch.lyrics.js";
 
 export async function extractJemCollection() {
-    await extractLyricsCollections("./output/jem", 1144, async (number, dir) => {
+    await extractLyricsCollections("./output/jem", 1151, async (number, dir) => {
         number = `${number}`.padStart(3, "0");
         const url = `https://www.jemaf.fr/v-jemaf-el/chant=jem${number}`;
         const formattedLyrics = await fetchFormattedLyrics(url, "JEM", extractLyricsFromJemaf);
